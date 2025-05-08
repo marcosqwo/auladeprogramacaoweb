b11bcbe287a8808f4b250aa85d926c5448e344cc
aluno <@marcosqwo>
2025-05-08 14:33:21 -0300

    aula dia 08/05 SweetAlert2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,153 +1,16 @@
 import { Component } from '@angular/core';
-
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template : '<button>(click)="showAlert()">Mostrar Alerta</button>'
 })
 export class AppComponent {
   title = 'project_U';
-/*
-CRIAÇÃO DE OBJETOS
-  gatinho:Gato;
-  franguinho:Galinha;
-  dog:Cachorro;
-
-  minhamoto:Moto;
-  meuCarro : Carro;
- conta : ContaBancaria;
-
-    fazersomdoanimal(animal:Animal){
-      animal.fazerbarulho();
-
-    }
-  meuanimal : Animal;
-
-
-  falaraforma(forma:Forma){
-    forma.calculararea();
+  showAlert() {
+    swal.fire('Olá!','SweetAlert2 Está Funcionando','success');
   }
-
-  redondo: Circulo;
-  retangular: Retangulo;
-
-  senhordosaneis : Livros;
-  pcacer : Eletronicos;
-  ovos:Alimentos;
-
-  person:Pessoa;
-  asalariado:Funcionario;
-
-
-
-
-
-
-
-
-  constructor() {
-
-    this.person = new Pessoa('joãozinho',15);
-
-    this.person.dadospessoa();
-
-    this.asalariado = new Funcionario('denilson',23,'gerente');
-
-    this.asalariado.mostrarcargo();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  this.senhordosaneis = new Livros('senhor dos aneis','fantasia',1200);
-  const valordesconto:number=this.senhordosaneis.CalcularDesconto(25);
-  console.log(valordesconto);
-
-  this.pcacer = new Eletronicos ('Pc da acer','computador de mesa',1200);
-  this.ovos = new Alimentos('ovo','ovo de galinha',25);
-
-  this.ovos.verificavalidade('28/03/2025')
-  this.pcacer.verificagarantia('20/03/2025');
-
-
-
-
-        this.redondo = new Circulo();
-        this.retangular = new Retangulo();
-
-        this.falaraforma(this.redondo);
-        this.falaraforma(this.retangular);
-
-
-  const meudog = new Cachorro("rex");
-  const meucat = new Gato("gatinho gatinho pspspsps");
-  this.meuanimal = new Animal("cavalo");
-
-
-  this.fazersomdoanimal(meudog);
-  this.fazersomdoanimal(meucat);
-  this.fazersomdoanimal(this.meuanimal);
-  console.log(this.meuanimal.getnome());
-
-
-
-  this.minhamoto= new Moto('cinza','honda fan');
-  this.minhamoto.setvelocidade(300);
-
-
-
-
-
-
-
-
-    this.gatinho = new Gato('geraldo',12);
-         this.franguinho = new Galinha('gertrudez', 2);
-         this.dog = new Cachorro('mauro milton', 2);
-
-         this.gatinho.setsom('miau');
-         this.dog.setsom('auau');
-         this.franguinho.setsom('piu piu');
-         this.dog.setdirecao('norte');
-         this.franguinho.setdirecao('sul')
-         this.gatinho.setdirecao('leste');
-
-
-
-
-
-
-    /!*this.meuCarro = new Carro('vermelho','civic');*!/
-/!*  console.log('esse é meu carro:',this.meuCarro.getcor());
-
-  console.log('esse é o modelo do meu carro:',this.meuCarro.getmoodelo());
-
-  const meuSedan = new Sedan('vermelho','accord',5);
-
-    meuSedan.abrirportamalas();*!/
-
-    /!*this.conta = new ContaBancaria(1523,1500,'marcos');
-    console.log(this.conta);
-    this.conta.sacar(500);*!/
-
-
-  }
-  mudarcor(): void {
-    this.meuCarro.setcor('azul');
-
-  }
-
-
-*/
 
 }
--- src/app/componente/visualizar-tarefa/visualizar-tarefa.component.ts
+++ src/app/componente/visualizar-tarefa/visualizar-tarefa.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Tarefaservice} from "../../app-core/service/tarefaservice.service";
 import {Tarefa} from "../../app-core/model/tarefa";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import Swal from "sweetalert2";
 declare var $: any;
 @Component({
   selector: 'app-visualizar-tarefa',
@@ -49,9 +50,21 @@ export class VisualizarTarefaComponent implements OnInit {
     if(this.form.valid){
       console.log(this.form.value);
       console.log("CADASTRO REALIZADO COM SUCESSO!");
-      this.form.reset();
+      let deuCerto = true;
+      if (deuCerto){
+        Swal.fire('Sucesso!',
+          'Tarefa Salva com sucesso',
+          'success');
+        this.form.reset();
+      } else {
+        Swal.fire('Não foi dessa vez!',
+          'Não foi possível salvar a tarefa!, Tente novamente mais tarde.',
+          'error')
+      }
     }else {
       console.log("Campos Invalidos Encontrados");
+      Swal.fire("Atenção!", "Alguns campos do formulário" +
+        " não estão corretos.", 'warning');
       this.marcartodoscomoclicados();
     }
 
